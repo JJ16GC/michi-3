690389f557d5d8038af65bd2b717d3d1c54a25bf
jj16gc <jj16gc@gmail.com>
2023-02-09 13:06:28 -0500

    michi :3

--- script.js
+++ script.js
@@ -3,21 +3,28 @@ function currentTime() {
   let dia = date.getDate();
   let mes = date.getMonth() + 1;
   let año = date.getFullYear();
+  let hh = date.getHours();
+  let mm = date.getMinutes();
+  let ss = date.getSeconds();
 
-  //hh = hh < 10 ? "0" + hh : hh;
-  //mm = mm < 10 ? "0" + mm : mm;
-  //ss = ss < 10 ? "0" + ss : ss;
+  hh = hh < 10 ? "0" + hh : hh;
+  mm = mm < 10 ? "0" + mm : mm;
+  ss = ss < 10 ? "0" + ss : ss;
 
   let fecha1 = moment("2022-10-12");
   console.log(fecha1);
   let fecha2 = moment(date);
   let diferencia = fecha2.diff(fecha1, "days");
+  let time = hh + ":" + mm + ":" + ss;
   let diferenciames = fecha2.diff(fecha1, "month");
-  let fecha = diferencia + " DIAS A TU LADO :3 \n Te amoooo";
+  let fecha = diferencia + " DIAS A TU LADO";
+  let reloj = time;
 
-  //let time = hh + ":" + mm + ":" + ss;
   let watch = document.querySelector("#watch");
+  let watch2 = document.querySelector("#figura");
   watch.innerHTML = fecha;
+  watch2.innerHTML = reloj;
+
 }
 
 setInterval(currentTime, 1000);
