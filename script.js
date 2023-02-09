532100e8c0f044881bd0e45baf6795b936b8bce7
jj16gc <jj16gc@gmail.com>
2023-02-09 12:49:53 -0500

    michi :3

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 function currentTime() {
-
-  moment.suppressDeprecationWarnings = true;
-
   let date = new Date();
   let dia = date.getDate();
   let mes = date.getMonth() + 1;
@@ -11,10 +8,12 @@ function currentTime() {
   //mm = mm < 10 ? "0" + mm : mm;
   //ss = ss < 10 ? "0" + ss : ss;
 
-  let fecha1 = "2022-10-12";
-  let fecha2 = moment(año + "-" + mes + "-" + dia);
+  let fecha1 = moment("2022-10-12");
+  console.log(fecha1);
+  let fecha2 = moment(date);
   let diferencia = fecha2.diff(fecha1, "days");
-  let fecha = diferencia + " DIAS A TU LADO :3";
+  let diferenciames = fecha2.diff(fecha1, "month");
+  let fecha = diferencia + " DIAS A TU LADO :3 \n Te amoooo";
 
   //let time = hh + ":" + mm + ":" + ss;
   let watch = document.querySelector("#watch");
