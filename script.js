ab6d35817fe6073391d456e9956df5989edca406
jj16gc <jj16gc@gmail.com>
2023-02-09 12:30:55 -0500

    michi :3

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 function currentTime() {
+
+  moment.suppressDeprecationWarnings = true;
+
   let date = new Date();
-  let hh = date.getHours();
-  let mm = date.getMinutes();
-  let ss = date.getSeconds();
   let dia = date.getDate();
-  let mes = date.getMonth()+1;
+  let mes = date.getMonth() + 1;
   let año = date.getFullYear();
 
   //hh = hh < 10 ? "0" + hh : hh;
   //mm = mm < 10 ? "0" + mm : mm;
   //ss = ss < 10 ? "0" + ss : ss;
 
-  let fecha1 = moment("2022-10-12");
+  let fecha1 = "2022-10-12";
   let fecha2 = moment(año + "-" + mes + "-" + dia);
   let diferencia = fecha2.diff(fecha1, "days");
   let fecha = diferencia + " DIAS A TU LADO :3";
 
   //let time = hh + ":" + mm + ":" + ss;
-  //let watch = document.querySelector("#watch");
-  //watch.innerHTML = fecha;
+  let watch = document.querySelector("#watch");
+  watch.innerHTML = fecha;
 }
 
 setInterval(currentTime, 1000);
