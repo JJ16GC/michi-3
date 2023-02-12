975e27871eb805f4112fe0e7819b1f52ca28726c
jj16gc <jj16gc@gmail.com>
2023-02-11 20:28:45 -0500

    michi

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function currentTime() {
-
   let date = new Date();
   let diae = date.getDate();
   let hh = date.getHours();
@@ -10,22 +9,28 @@ function currentTime() {
   mm = mm < 10 ? "0" + mm : mm;
   ss = ss < 10 ? "0" + ss : ss;
 
+  let fecha = "Errorxd";
+  let reloj = "Errorxd";
   let fecha1 = moment("2022-10-12");
   let diaespecial = fecha1.date();
   let fecha2 = moment(date);
-
-  if (diae == diaespecial) {
-    ani =
-      "<button id='boton'><a href='mensaje.html'>Clickeame chiquita :3</a></button>";
-  } else {
-    ani = "Y contando... ";
-  }
-
   let diferencia = fecha2.diff(fecha1, "days");
   let time = hh + ":" + mm + ":" + ss;
   let diferenciames = fecha2.diff(fecha1, "month");
-  let fecha = "" + diferenciames + " MESES<br> " + diferencia + " DÍAS ";
-  let reloj = time;
+
+  /* DIA ESPECIAL */
+
+  if (10 == 10) {
+    ani =
+      "<button id='boton'><a href='mensaje.html'>Ver Carta 💌</a></button>";
+    fecha = "Holi Amor Felices " + diferenciames + " meses!!!<br> ";
+    reloj = "<a href='mensaje.html'>Ver Galeria 7u7</a>";
+  } else {
+    /* */
+    ani = "Y contando... ";
+    fecha = "" + diferenciames + " MESES<br> " + diferencia + " DÍAS ";
+    reloj = time;
+  }
 
   let watch = document.querySelector("#watch");
   let watch2 = document.querySelector("#figura");
@@ -34,8 +39,6 @@ function currentTime() {
   watch.innerHTML = fecha;
   watch2.innerHTML = reloj;
   watch3.innerHTML = ani;
-
-  
 }
 
 setInterval(currentTime, 1000);
